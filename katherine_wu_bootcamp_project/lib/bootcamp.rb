7c62a2aa7ada6399db1eb3a74972058db381ac8d
kw-8 <kwu11235813@gmail.com>
2021-08-16 11:27:53 -0700

    getters done, hire done

--- katherine_wu_bootcamp_project/lib/bootcamp.rb
+++ katherine_wu_bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,34 @@
 class Bootcamp
-  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new([])
+  end
+
+  def name
+    @name
+  end
+  def slogan
+    @slogan
+  end
+  def teachers
+    @teachers
+  end
+  def students
+    @students
+  end
+  def hire(teacher)
+    @teachers << teacher
+  end
+  def enroll(student)
+    if @students.length < student_capacity
+        @students << student
+        return true
+    else return false
+    end
+  end
+
 end
